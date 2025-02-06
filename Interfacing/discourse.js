1358ad66e538a98304fff9187ff118786100ee55
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2025-02-05 20:53:29 -0500

    more info

--- Interfacing/discourse.js
+++ Interfacing/discourse.js
@@ -21,10 +21,14 @@ exports.Validate = function (payload, sig, userdata) {
             "nonce": nonce,
             "external_id": userdata.Id,
             "email": userdata.Email,
-            "username": userdata.Username
+            "username": userdata.Username,
+            "avatar_url": userdata.Bio.PfpURL,
+            "bio": userdata.Bio.Description,
+            "moderator": userdata.Rank >= 4,
+            "admin": userdata.Rank >= 5
         };
         // TODO: Probably make better
-        if(userdata.Bio !== null && userdata.Bio !== undefined && userdata.Bio.DisplayName !== null && userdata.Bio.DisplayName !== undefined && userdata.Bio.DisplayName !== ""){
+        if(userdata.Bio.DisplayName !== null && userdata.Bio.DisplayName !== undefined && userdata.Bio.DisplayName !== ""){
             d.name = userdata.Bio.DisplayName
         }
         else{
